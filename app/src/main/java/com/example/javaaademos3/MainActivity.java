70f346322bb5d284f71d6cdcfc81c3c5611c892c
pradeep71d <pkdubey071@gmail.com>
2020-03-14 14:57:01 +0530

    add

---
+++ app/src/main/java/com/example/javaaademos3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.javaaademos3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            System.out.println("A");
+            int num = 98 / 0;
+            System.out.println("B");
+        } catch (ArithmeticException e) {
+            System.out.println("C");
+        } catch (Exception e) {
+            System.out.println("D");
+        } finally {
+            System.out.println("E");
+        }
+        System.out.println("F");
+    }
+}
